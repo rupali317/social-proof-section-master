988a18d12e9e405d5d1692096f85e649580a1de2
PriyankaRC16 <rupali317@gmail.com>
2023-12-31 17:21:57 +0800

    Completed the footer and linked them to the respective files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Footer } from "./components/Footer";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const main = ReactDOM.createRoot(document.getElementById("main"));
+const footer = ReactDOM.createRoot(document.getElementById("footer"));
+
+main.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+footer.render(
+  <React.StrictMode>
+    <Footer />
   </React.StrictMode>
 );
 
