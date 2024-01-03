32c4955e5efe7204e8d245032f601653a500954f
PriyankaRC16 <rupali317@gmail.com>
2024-01-03 18:40:44 +0800

    Refactoring + Formatting

--- src/components/WelcomeMessage/WelcomeMessageData.js
+++ src/components/WelcomeMessage/WelcomeMessageData.js
@@ -3,5 +3,5 @@ export const WelcomeMessageData = {
     title: "10,000+ of our users love our products.",
     subtitle:
       "We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.",
-  }
+  },
 };
