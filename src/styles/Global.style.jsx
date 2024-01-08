7b54ef81559841c107808bf72610587f96e067a2
PriyankaRC16 <rupali317@gmail.com>
2024-01-08 19:45:43 +0800

    Updated the screen-reader-only class with the industry standards

--- src/styles/Global.style.jsx
+++ src/styles/Global.style.jsx
@@ -218,7 +218,15 @@ export const DefaultStyles = createGlobalStyle`
   }
 
   .screen-reader-only {
-    clip-path: polygon(0 0, 0 0, 0 0, 0 0)
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0,0,0,0);
+    white-space: nowrap;
+    border-width: 0;
   }
 
   @media (min-width: ${DesktopWidth}) {
